434e37085588439d38a53874492e42e6afd25074
sw9504 <sladewilson9504@gmail.com>
2022-09-27 16:35:14 -0300

    Add settings screen though new activity

---
+++ app/src/main/java/com/utn/practicebottombar/activities/SettingsActivity.kt
@@ -0,0 +1,25 @@
+package com.utn.practicebottombar.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.utn.practicebottombar.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
--- app/src/main/java/com/utn/practicebottombar/fragments/CFragment.kt
+++ app/src/main/java/com/utn/practicebottombar/fragments/CFragment.kt
@@ -6,14 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.utn.practicebottombar.R
 
 class CFragment : Fragment() {
+    lateinit var v : View
+    lateinit var button : Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_c, container, false)
+        v = inflater.inflate(R.layout.fragment_c, container, false)
+
+        button = v.findViewById(R.id.button)
+
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        button.setOnClickListener {
+            val action = CFragmentDirections.actionCFragmentToSettingsActivity()
+            v.findNavController().navigate(action)
+        }
+
     }
 
 }
